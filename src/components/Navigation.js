8fa1a189ec0c864f4d9997e01fa95c3b318e1d37
nailah <nailahbrown.cs@gmail.com>
2026-04-22 22:45:51 -0400

    Colorful name card and header/footer
    I added a colorful name, as well as a header and a footer to add some pizazz to the page. The footer has my contact information and the header allows you to navigate the page and hosts the colorful name card.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 import "./Footer.css";
+import { AuroraTextEffect } from "./lightswind/aurora-text-effect.tsx"
 
 export default function Navigation() {
     return (
         <nav className="navigation">
             <div className="nav-container">
-                <h2 className="nav-title">Nailah Brown</h2>
+                <AuroraTextEffect 
+                    text="Nailah Brown" 
+                    fontSize="60px"
+                    colors={{
+                        first: "#abcdff",
+                        second: "#4781d7",
+                        third: "#d9abff",
+                        fourth: "#955ee2"
+                    }}
+                    blurAmount="blur-lg"
+                />
                 <ul className="nav-links">
                     <li><Link to="/">Projects</Link></li>
                     <li><Link to="/about">About Me</Link></li>
